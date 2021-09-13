271c3859f58d92179e822967ed2ca4aa14b1d660
x318 <jare854@gmail.com>
2021-09-14 00:47:10 +0300

    Generated tests removed

--- server/src/auth/constants.ts
+++ server/src/auth/constants.ts
@@ -1,5 +1,5 @@
 export const jwtConstants = {
   secret:
     'ad402cd27eae099d1a9a8b3aed9dd5107e34d32c5e29575ea5cc394c0c28daceafc4d71d7feb00d8e9a33eaf80ade3850666e5b6c15d549b10448b4ffec7dbd3',
-  expiresIn: '60s',
+  expiresIn: '30m',
 };
